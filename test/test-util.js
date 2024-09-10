543481b7063ef3d84495062247b5afd4144aff85
Ramdan Syaripudin <macbook@Macbook.local>
2024-09-10 22:54:39 +0700

    feat: add api get/put/delete/search contacts

--- test/test-util.js
+++ test/test-util.js
@@ -35,3 +35,37 @@ export const removeAllTestContacts = async () => {
     },
   });
 };
+
+export const createTestContact = async () => {
+  await prismaClient.contact.create({
+    data: {
+      username: "test",
+      first_name: "test",
+      last_name: "test",
+      email: "test@mail.com",
+      phone: "082123456789",
+    },
+  });
+};
+
+export const createManyTestContacts = async () => {
+  for (let i = 0; i < 15; i++) {
+    await prismaClient.contact.create({
+      data: {
+        username: `test`,
+        first_name: `test ${i}`,
+        last_name: `test ${i}`,
+        email: `test${i}@mail.com`,
+        phone: `082123456789${i}`,
+      },
+    });
+  }
+};
+
+export const getTestContact = async () => {
+  return prismaClient.contact.findFirst({
+    where: {
+      username: "test",
+    },
+  });
+};
