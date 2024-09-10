26bebb61fb887aaec01348deac94c50f7917b2b1
Ramdan Syaripudin <macbook@Macbook.local>
2024-09-10 22:02:28 +0700

    feat: add api contact

--- src/route/api.js
+++ src/route/api.js
@@ -1,12 +1,17 @@
 import express from "express";
 import userController from "../controller/user-controller.js";
+import contactController from "../controller/contact-controller.js";
 import { authMiddleware } from "../middleware/auth-middlware.js";
 
 const userRouter = express.Router();
 userRouter.use(authMiddleware);
 
+// User API
 userRouter.get("/api/users/current", userController.get);
 userRouter.patch("/api/users/current", userController.update);
 userRouter.delete("/api/users/logout", userController.logout);
+
+//Contact API
+userRouter.post("/api/contacts", contactController.create);
 
 export { userRouter };
